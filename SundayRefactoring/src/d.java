50cd4595865a93df517c480d308a512e28d1e833
M.L.Akila Liyanage <IT19120812@my.sliit.lk>
2021-08-08 14:05:12 +0530

    init

---
+++ SundayRefactoring/src/d.java
@@ -0,0 +1,28 @@
+
+
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+
+public class d {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		a a1 = new a();
+		try {
+			c3.rEQUESTtRANSFORM();
+			a1.a2();
+			a1.a3();
+			a1.a4();
+//			employeeService.eMPLOYEEGETBYID("EMP10004");
+//			employeeService.EMPLOYEEDELETE("EMP10001");
+			a1.a5();
+		} catch (Exception e) {
+		}
+
+	}
+
+}
